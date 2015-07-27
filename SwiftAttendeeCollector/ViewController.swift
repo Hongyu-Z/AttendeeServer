95b2051df2a43f5ed0bc1cdd1613f01556700cbd
HongyuZhu <Hongyu.Zhu@concur.com>
2015-07-27 15:23:13 -0700

    first commit

---
+++ SwiftAttendeeCollector/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftAttendeeCollector
+//
+//  Created by Zhu, Hongyu on 7/27/15.
+//  Copyright (c) 2015 Zhu, Hongyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("aaa")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
